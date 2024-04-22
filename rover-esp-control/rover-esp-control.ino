357740993f7f5402ec7ea75b16de668fb65d83e8
G2-Games <ke0bhogsg@gmail.com>
2024-04-22 01:29:23 -0500

    Refactored and improved code

--- rover-esp-control/rover-esp-control.ino
+++ rover-esp-control/rover-esp-control.ino
@@ -1,20 +1,20 @@
-// Bluetooth
+/* Bluetooth */
 #include "BluetoothSerial.h"
 
-// Camera
+BluetoothSerial SerialBT;
+
+/* CAMERA SETUP STUFF */
 #include "esp_camera.h"
 #include "FS.h"
 #include "SD_MMC.h"
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 #include "driver/rtc_io.h"
-#include <EEPROM.h>
-
-BluetoothSerial SerialBT;
+#include "EEPROM.h"
 
 #define EEPROM_SIZE 1
 
-// Pin definition for CAMERA_MODEL_AI_THINKER
+// Pin definitions for CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -33,8 +33,20 @@ BluetoothSerial SerialBT;
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
 
-const String DEVICE_NAME = "UNL-Rover";
-const String MAC = "08:F9:E0:ED:20:4A";
+/* RoboClaw */
+#include "RoboClaw.h"
+
+// Pin and address definitions for the RoboClaw
+#define RXD2 16
+#define TXD2 0
+
+RoboClaw rc = RoboClaw(&Serial2, 10000);
+
+#define addresssRc 0x80 // 128
+
+#define DEVICE_NAME "UNL-Rover"
+#define MAC "08:F9:E0:ED:20:4A"
+#define PIC_NUM_ADDR 0
 
 int pictureNumber = 0;
 camera_fb_t *fb = NULL;
@@ -44,16 +56,136 @@ enum Error {
     EXPECTED_ARG = -2,
     FUNCTION_FAILURE = -3,
     INVALID_ARG = -4,
-}; 
+};
+
+enum Direction {
+    FORWARD = 1,
+    BACKWARD = 2,
+};
 
 void setup() {
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+    // Initialize serial
     Serial.begin(115200);
 
-    // Set up bluetooth
-    SerialBT.begin(DEVICE_NAME);
-    Serial.printf("Bluetooth Device \"%s\" started; MAC Address: %s\n", DEVICE_NAME.c_str(), MAC.c_str());
+    // Initalize RoboClaw
+    rc.begin(38400);
+    rc.ForwardBackwardM1(addresssRc, 64);
+    rc.ForwardBackwardM2(addresssRc, 64);
 
+    // Initalize bluetooth serial
+    SerialBT.begin(DEVICE_NAME);
+    Serial.printf("Bluetooth Device \"%s\" started; MAC Address: %s\n", DEVICE_NAME, MAC);
+
+    // Initalize camera
+    if (!initCamera(framesize_t::FRAMESIZE_UXGA)) {
+        Serial.println("Camera could not be started");
+
+        ESP.restart();
+        return;
+    }
+
+    // Initalize SD card
+    if(!SD_MMC.begin()){
+        Serial.println("SD Card Mount Failed");
+
+        ESP.restart();
+        return;
+    }
+
+    // Initalize EEPROM
+    EEPROM.begin(EEPROM_SIZE);
+    pictureNumber = EEPROM.read(PIC_NUM_ADDR);
+}
+
+void loop() {
+    // Wait around if not connected to a bluetooth device
+    if (!SerialBT.connected()) {
+        delay(20);
+        return;
+    }
+
+    // Check for new messages and restart the loop if there are none
+    if (!SerialBT.available()) {
+        delay(20);
+        return;
+    }
+
+    // Get the string from the terminal
+    String result = SerialBT.readStringUntil('\n');
+    if (result.length() == 0) {
+        SerialBT.write(Error::INVALID);
+        return;
+    }
+
+    // Parse the arguments and perform some actions
+    int status = parseArgs(result);
+
+    // Return the length of the sent bytes, indicating a normal success
+    SerialBT.write(status);
+}
+
+int parseArgs(String input) {
+    // Get the first argument
+    char *result_char = const_cast<char*> (input.c_str());
+    char *arg = strtok(result_char, " ");
+
+    // Parse the arguments
+    switch (*arg) {
+        // Move the rover
+        case('m'): {
+            Serial.println("Moving");
+
+            arg = strtok(NULL, " ");
+            if (arg == NULL) {
+                return Error::EXPECTED_ARG;
+            }
+
+            int direction = atoi(arg);
+            if (direction == 0) {
+                return Error::INVALID_ARG;
+            }
+
+            rc.ForwardBackwardM1(addresssRc, 64);
+        }
+        break;
+
+        // Save picture
+        case('p'): {
+            if (!savePicture()) {
+                return Error::FUNCTION_FAILURE;
+            }
+        }
+        break;
+
+        // Set the next image number
+        case('n'): {
+            arg = strtok(NULL, " ");
+            if (arg == NULL) {
+                return Error::EXPECTED_ARG;
+            }
+
+            int newNumber = atoi(arg);
+            if (newNumber == 0) {
+                return Error::INVALID_ARG;
+            }
+
+            pictureNumber = newNumber;
+            EEPROM.write(PIC_NUM_ADDR, newNumber);
+            EEPROM.commit();
+        }
+        break;
+
+        // If nothing else matched, it must be wrong!
+        default: {
+            return Error::INVALID;
+        }
+    }
+
+    return input.length() + 1;
+}
+
+/// Initalizes the camera
+bool initCamera(framesize_t frameSize) {
     // Set up camera
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
@@ -77,120 +209,31 @@ void setup() {
     config.xclk_freq_hz = 20000000;
     config.pixel_format = PIXFORMAT_JPEG;
 
-    // If there is PSRAM, the camera can take higher quality photos
-    if(psramFound()){
-        config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-        config.jpeg_quality = 10;
-        config.fb_count = 2;
-    } else {
-        config.frame_size = FRAMESIZE_SVGA;
-        config.jpeg_quality = 12;
-        config.fb_count = 1;
-    }
+    config.frame_size = frameSize; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
 
     // Initalize camera and framebuffer
     esp_err_t err = esp_camera_init(&config);
     if (err != ESP_OK) {
-        Serial.printf("Camera init failed with error 0x%x", err);
-
-        ESP.restart();
-        return;
+        return false;
     }
 
-    // Initalize SD card
-    if(!SD_MMC.begin()){
-        Serial.println("SD Card Mount Failed");
-        
-        ESP.restart();
-        return;
-    }
-
-    // Initalize EEPROM
-    EEPROM.begin(EEPROM_SIZE);
-    pictureNumber = EEPROM.read(0);
-}
-
-void loop() {
-    if (!SerialBT.connected()) {
-        delay(100);
-        return;
-    }
-
-    String result = "";
-    int resLen = 0;
-    if (SerialBT.available()) {
-        result = SerialBT.readStringUntil('\n');
-        Serial.println(result);
-        if (result.length() == 0) {
-            SerialBT.write(Error::INVALID);
-            return;
-        }
-
-        resLen = result.length() + 1;
-    }
-
-    if (resLen != 0) {
-        // Get the first argument
-        char *result_char = const_cast<char*> (result.c_str()); 
-        char *arg = strtok(result_char, " "); 
-
-        if (*arg == 'm') {
-            Serial.println("Moving");
-        } else if (*arg == 'p') {
-            int times = 0;
-            arg = strtok(NULL, " ");
-            if (arg == NULL) {
-                times = 1;
-            } else {
-                times = atoi(arg);
-            }
-
-            if (times == 0) {
-                SerialBT.write(Error::INVALID_ARG);
-                return;
-            }
-
-            for (int i = 0; i < times; i++) {
-                if (!savePicture()) {
-                    SerialBT.write(Error::FUNCTION_FAILURE);
-                    return;
-                }
-            }
-        } else if (*arg == 'n') {
-            arg = strtok(NULL, " ");
-            if (arg == NULL) {
-                SerialBT.write(Error::EXPECTED_ARG);
-                return;
-            }
-
-            int newVal = atoi(arg);
-            if (newVal == 0) {
-                SerialBT.write(Error::INVALID_ARG);
-                return;
-            }
-
-            pictureNumber = newVal;
-            EEPROM.write(0, newVal);
-            EEPROM.commit();
-        } else {
-            SerialBT.write(Error::INVALID);
-            return;
-        }
-
-        // Return the length of the sent bytes, success
-        SerialBT.write(result.length() + 1);
-    }
-}
-
-bool savePicture() {
-    for (int i = 0; i < 3; i++) {
+    // Get 5 frames from the camera to warm up
+    for (int i = 0; i < 5; i++) {
         fb = esp_camera_fb_get();
         esp_camera_fb_return(fb);
         fb = NULL;
     }
+
+    return true;
+}
+
+/// Saves a picture to the next position
+bool savePicture() {
     fb = esp_camera_fb_get();
 
-    String path = "/pic" + String(pictureNumber) +".jpeg";
+    String path = "/pic" + leadingZeroes(pictureNumber) +".jpeg";
     fs::FS &fs = SD_MMC;
 
     File file = fs.open(path.c_str(), FILE_WRITE);
@@ -202,15 +245,35 @@ bool savePicture() {
 
     file.write(fb->buf, fb->len); // payload (image), payload length
     Serial.printf("Saved file to path: %s\n", path.c_str());
+    while (!file.available()) {
+        delay(5);
+    }
+    file.flush();
     file.close();
 
-    EEPROM.write(0, pictureNumber);
+    pictureNumber++;
+    EEPROM.write(PIC_NUM_ADDR, pictureNumber);
     EEPROM.commit();
 
     esp_camera_fb_return(fb);
-    pictureNumber++;
 
     return true;
+}
+
+/// Adds leading zeroes to an integer, and returns it as a string
+String leadingZeroes(int input) {
+    String number = String(input);
+    String newNumber = "";
+
+    if (input < 10) {
+        newNumber = "000";
+    } else if (input < 100) {
+        newNumber = "00";
+    } else if (input < 1000) {
+        newNumber = "0";
+    }
+
+    return newNumber + number;
 }
 
 
